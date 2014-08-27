47dcf4cefefab18c98c5a7217c9240f27e411509
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2014-08-27 09:07:11 +0200

    production oath callback

--- config/env/production.js
+++ config/env/production.js
@@ -28,6 +28,6 @@ module.exports = {
 	google: {
 		clientID: process.env.GOOGLE_ID || 'APP_ID',
 		clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
-		callbackURL: 'http://localhost:3000/auth/google/callback'
+		callbackURL: 'http://xskillz.herokuapp.com/auth/google/callback'
 	}
 };
