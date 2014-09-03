cec3f13e4ece3540268787aa5a936c1fca9049d0
yannick grenzinger <yannick.grenzinger@gmail.com>
2014-09-03 14:54:07 +0200

    Refactoring + autocomplete

--- public/modules/users/controllers/users.client.controller.js
+++ public/modules/users/controllers/users.client.controller.js
@@ -38,6 +38,25 @@ angular.module('users').controller('UsersController', ['_', '$scope', '$http', '
 		$scope.skillz = [];
 
 		$scope.results = [];
+		var transformResultToSkillz = function(response) {
+			return _.map(response.data, function(node){
+				return {'name' : node[0].data.name, 'level' : node[1].data.level , 'like' : node[1].data.like, relationId : node[1].self };
+			});
+		};
+		var transformResultToXebians = function(response) {
+			return _.map(response.data, function(node){
+				return {
+					'skillName': node[2].data.name,
+					'email': node[0].data.email,
+					'picture': node[0].data.picture,
+					'displayName' : node[0].data.displayName,
+					'firstName' : node[0].data.firstName,
+					'lastName' : node[0].data.lastName,
+					'level' : node[1].data.level ,
+					'like' : node[1].data.like,
+					relationId : node[1].self };
+			});
+		};
 
 		$http.get('/users/me/skillz').then(function(response){
 			$scope.skillz = _.map(response.data, function(node){
@@ -50,31 +69,39 @@ angular.module('users').controller('UsersController', ['_', '$scope', '$http', '
 
 		$scope.removeRelation = function(relationId){
 			$http.post('users/me/skillz/disassociate', {'relationship': relationId} ).then(function(response){
-				$scope.skillz = _.map(response.data, function(node){
-					return {'name' : node[0].data.name, 'level' : node[1].data.level , 'like' : node[1].data.like, relationId : node[1].self };
-				});
+				$scope.skillz = transformResultToSkillz(response);
 			});
 		};
 
 		// List all xebians corresponding to some skillz
-		$scope.search = function(){
+
+		$scope.changingSearchSkillz = function() {
+				if ($scope.query.length > 2) {
+					$scope.searchSkillz();
+				}
+		};
+		$scope.searchSkillz = function(){
 			$http.get('/skillz', {'params': {'q':$scope.query}})
 				.then(function(response){
-					$scope.results = response.data;
+					$scope.results = transformResultToXebians(response);
 			});
 		};
 
 		// Affect a skill to current user
 		$scope.associateSkill = function() {
 			if ( ! (_.find($scope.skillz, function(skill){return skill.name === $scope.newSkill;} ))) {
-					$http.put('users/me/skillz', { 'skill': {'name': $scope.newSkill}, 'relation_properties': {'level' : $scope.level , 'like' : $scope.like }}).then(function(response) {
-						$scope.skillz = _.map(response.data, function(node){
-							return {'name' : node[0].data.name, 'level' : node[1].data.level , 'like' : node[1].data.like, relationId : node[1].self };
-						});
-				});
+					$http.put('users/me/skillz', { 'skill': {'name': $scope.newSkill}, 'relation_properties': {'level' : $scope.level , 'like' : $scope.like }})
+						.then(function(response) {
+								$scope.skillz = transformResultToSkillz(response);
+					});
 			}
 		};
 
+		$scope.changingSearchXebians = function() {
+				if ($scope.query.length > 2) {
+					$scope.searchXebians();
+				}
+		};
 		$scope.searchXebians = function(){
 			$http.get('/users', {'params': {'q':$scope.query}})
 				.then(function(response){
