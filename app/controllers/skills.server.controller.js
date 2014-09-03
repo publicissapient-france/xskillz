4c63511c0ccdd6e82b8026f3b87bd0a3b801cbad
Alexis Kinsella <alexis.kinsella@gmail.com>
2014-09-03 16:56:56 +0200

    Fixed cloud tag

--- app/controllers/skills.server.controller.js
+++ app/controllers/skills.server.controller.js
@@ -4,9 +4,14 @@
  * Module dependencies.
  */
 var xskillzNeo4J = require('../models/xskillz.neo4j');
+var _ = require('underscore');
 
 exports.all = function(req,res){
 	xskillzNeo4J.findAllSkills().then(function(result) {
+		result = _(result).filter(function(r) {
+			return r[1] > 0;
+		});
+
 		res.jsonp(result);	
 	});
 	
