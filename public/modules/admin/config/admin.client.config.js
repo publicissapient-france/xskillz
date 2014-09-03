57a08c83fa5bfff784dc073f04c53990c5261a2f
Alexis Kinsella <alexis.kinsella@gmail.com>
2014-09-03 10:40:12 +0200

    Remove of admin UI

--- public/modules/admin/config/admin.client.config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Configuring the Articles module
-angular.module('admin').run(['Menus',
-	function(Menus) {
-		Menus.addSubMenuItem('topbar', 'admin', 'Admin', 'admin');
-	}
-]);
--- public/modules/admin/controllers/admin.client.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Admins controller
-angular.module('admin').controller('AdminController', ['$scope', '$stateParams', '$location', 'Authentication', 'Admin',
-	function($scope, $stateParams, $location, Authentication, Admin ) {
-		$scope.authentication = Authentication;
-	}
-]);
