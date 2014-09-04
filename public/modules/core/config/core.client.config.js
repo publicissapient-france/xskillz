bc4a069757b1195cb748a6f61d07f5f951508333
Julien Smadja <julien.smadja@gmail.com>
2014-09-04 20:43:19 +0200

    Add d3 for tagcloud page

--- public/modules/core/config/core.client.config.js
+++ public/modules/core/config/core.client.config.js
@@ -7,7 +7,7 @@ angular.module('core').run(['Menus',
 		Menus.addMenuItem('topbar', 'My skills', 'myskillz', 'item');
 		Menus.addMenuItem('topbar', 'Search for skills', 'skillz/search', 'item');
 		Menus.addMenuItem('topbar', 'Search for xebians', 'users/search', 'item');
-		Menus.addMenuItem('topbar', 'VIZ', 'skillz/cloudtag', 'item');
+		Menus.addMenuItem('topbar', 'Dataviz', 'skillz/cloudtag', 'item');
 		Menus.addMenuItem('topbar', 'Skillz management', 'skillz/management', 'item');
 	}
 ]);
