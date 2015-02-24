58a4773fa2b396e8905a5fd6dc24f847ff1a556b
Julien Smadja <julien.smadja@gmail.com>
2015-02-24 21:43:16 +0100

    Add Gravatars

--- public/config.js
+++ public/config.js
@@ -15,7 +15,7 @@ var ApplicationConfiguration = (function() {
 
 	// Init module configuration options
 	var applicationModuleName = 'xskillz';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'lodash', 'd3', 'angulartics', 'angulartics.google.analytics'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'lodash', 'd3', 'angulartics', 'angulartics.google.analytics', 'ui.gravatar'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName) {
