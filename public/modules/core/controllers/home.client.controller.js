b3e0f797e5e801108d83f8b23b05d634456087d0
Julien Smadja <julien.smadja@gmail.com>
2015-03-06 17:41:05 +0100

    Fix security issue on Home Page

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -10,22 +10,25 @@ angular.module('core').directive('notification', function () {
     };
 });
 
-angular.module('core').filter('levelName', function() {
-    return function(input) {
-        if(input == 0) return 'intéressé(e)';
-        if(input == 1) return 'débutant(e)';
-        if(input == 2) return 'confirmé(e)';
-        if(input == 3) return 'expert(e)';
+angular.module('core').filter('levelName', function () {
+    return function (input) {
+        if (input == 0) return 'intéressé(e)';
+        if (input == 1) return 'débutant(e)';
+        if (input == 2) return 'confirmé(e)';
+        if (input == 3) return 'expert(e)';
     };
 });
 
 angular.module('core').controller('HomeController', ['$scope', '$http', '$location', 'Users', 'Authentication',
     function ($scope, $http, $location, Users, Authentication) {
 
-        $http.get('/users/notifications')
-            .then(function (response) {
-                $scope.notifications = response.data;
-            });
+        if (Authentication.user) {
+            $http.get('/users/notifications')
+                .then(function (response) {
+                    $scope.notifications = response.data;
+                });
+        }
+
 
         $scope.deleteAccount = function () {
             var user = new Users(Authentication.user);
