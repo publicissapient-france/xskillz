76409886b58958ffafd2fcd4221c1dc283ec6ec4
jsmadja <julien.smadja@gmail.com>
2015-06-19 11:27:23 +0200

    #57 Pouvoir classer les skillz

--- public/modules/core/config/core.client.config.js
+++ public/modules/core/config/core.client.config.js
@@ -7,6 +7,7 @@ angular.module('core').run(['Menus',
 		Menus.addMenuItem('topbar', 'Mon profil', 'myskillz', 'item', null, false, ['XEBIAN']);
 		Menus.addMenuItem('topbar', 'Chercher une compétence', 'skillz/search', 'item', null, false, ['XEBIAN']);
 		Menus.addMenuItem('topbar', 'Chercher un Allié', 'users/search', 'item', null, false, ['XEBIAN']);
+		Menus.addMenuItem('topbar', 'Classer les compétences', 'skillz/ordering', 'item', null, false, ['XEBIAN']);
 		Menus.addMenuItem('topbar', 'Dataviz', 'skillz/cloudtag', 'item', null, false, ['XEBIAN']);
 		Menus.addMenuItem('topbar', 'Administration', 'skillz/management', 'item', null, false, ['MANAGER']);
 	}
--- public/modules/skillz/config/skillz.client.routes.js
+++ public/modules/skillz/config/skillz.client.routes.js
@@ -2,20 +2,24 @@
 
 // Setting up route
 angular
-	.module('skillz').config(['$stateProvider',
-	function($stateProvider) {
-		// Users state routing
-		$stateProvider.
-		state('skillz/search',{
-			url: '/skillz/search',
-			templateUrl: 'modules/skillz/views/search.client.view.html'
-		}).
-		state('skillz/cloudtag',{
-			url: '/skillz/cloudtag',
-			templateUrl : 'modules/skillz/views/cloudtag.client.view.html',
-		}).
-		state('skillz/management',{
-			url: '/skillz/management',
-			templateUrl: 'modules/skillz/views/management.client.view.html'
-		});
-	}]);
+    .module('skillz').config(['$stateProvider',
+        function ($stateProvider) {
+            // Users state routing
+            $stateProvider.
+                state('skillz/search', {
+                    url: '/skillz/search',
+                    templateUrl: 'modules/skillz/views/search.client.view.html'
+                }).
+                state('skillz/cloudtag', {
+                    url: '/skillz/cloudtag',
+                    templateUrl: 'modules/skillz/views/cloudtag.client.view.html'
+                }).
+                state('skillz/ordering', {
+                    url: '/skillz/ordering',
+                    templateUrl: 'modules/skillz/views/ordering.client.view.html'
+                }).
+                state('skillz/management', {
+                    url: '/skillz/management',
+                    templateUrl: 'modules/skillz/views/management.client.view.html'
+                });
+        }]);
