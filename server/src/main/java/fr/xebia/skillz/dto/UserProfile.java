f1a4a5ac0b757c84b87dbc7d2ffb6f56461be34b
jsmadja <julien.smadja@gmail.com>
2015-09-30 09:00:16 +0200

    Refactoring

--- server/src/main/java/fr/xebia/skillz/dto/UserProfile.java
+++ server/src/main/java/fr/xebia/skillz/dto/UserProfile.java
@@ -1,11 +1,14 @@
 package fr.xebia.skillz.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import fr.xebia.skillz.model.Domain;
 import fr.xebia.skillz.model.User;
 import fr.xebia.skillz.model.UserSkill;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
 import static java.util.stream.Collectors.toList;
 
 public class UserProfile extends BasicUserProfile {
@@ -31,6 +34,46 @@ public class UserProfile extends BasicUserProfile {
                         .stream()
                         .filter(s -> s.hasDomain(domain)).collect(toList()))));
         return userDomains;
+    }
+
+
+    private static class UserDomain {
+        private Domain domain;
+        private List<UserSkill> userSkills;
+
+        public UserDomain(Domain domain, List<UserSkill> collect) {
+            this.domain = domain;
+            this.userSkills = collect;
+        }
+
+        public String getName() {
+            return domain.getName();
+        }
+
+        public List<DomainSkill> getSkills() {
+            return userSkills.stream().map(DomainSkill::new).collect(toList());
+        }
+    }
+
+    @JsonInclude(NON_EMPTY)
+    private static class DomainSkill {
+        private final UserSkill userSkill;
+
+        public DomainSkill(UserSkill userSkill) {
+            this.userSkill = userSkill;
+        }
+
+        public String getName() {
+            return userSkill.getSkill().getName();
+        }
+
+        public int getLevel() {
+            return userSkill.getLevel();
+        }
+
+        public boolean isInterested() {
+            return userSkill.isInterested();
+        }
     }
 
 }
