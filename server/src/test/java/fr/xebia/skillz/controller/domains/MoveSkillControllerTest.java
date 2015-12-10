f658ec6f4a53a4944026d9e8f072753dc0bcfa92
jsmadja <julien.smadja@gmail.com>
2015-12-10 18:14:28 +0100

    Move skill from one domain to another

---
+++ server/src/test/java/fr/xebia/skillz/controller/domains/MoveSkillControllerTest.java
@@ -0,0 +1,39 @@
+package fr.xebia.skillz.controller.domains;
+
+
+import fr.xebia.skillz.controller.skills.GetSkillsController;
+import fr.xebia.skillz.dto.BasicSkillProfile;
+import fr.xebia.skillz.model.Domain;
+import fr.xebia.skillz.model.Skill;
+import fr.xebia.skillz.repository.DomainRepository;
+import fr.xebia.skillz.repository.TransactionSkillzTest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static fr.xebia.skillz.model.Company.XEBIA;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MoveSkillControllerTest extends TransactionSkillzTest {
+
+    @Autowired
+    private MoveSkillController moveSkillController;
+
+    @Autowired
+    private GetSkillsController skillsController;
+
+    @Autowired
+    private DomainRepository domainRepository;
+
+    @Test
+    public void should_move_skill_from_foundation_to_another() {
+        BasicSkillProfile skillToMove = skillsController.getMatchingSkillsFromCompany(XEBIA.getId(), "Java").iterator().next();
+        assertThat(skillToMove.getDomain().getName()).isEqualTo("Craft");
+
+        Domain domain = domainRepository.findAllByCompany(XEBIA).stream().filter(d -> d.getName().equals("Back")).findFirst().get();
+
+        moveSkillController.move(domain.getId(), new Skill(skillToMove.getId(), "Java", XEBIA));
+
+        assertThat(skillToMove.getDomain().getName()).isEqualTo("Back");
+    }
+
+}
