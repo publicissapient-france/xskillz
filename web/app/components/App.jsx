821fffee30b33c6a9112e8addfd6e7c585c26275
Evgeny Yurchuk <evgeny.yurchuk@gmail.com>
2016-01-26 01:25:06 +0100

    First design failed, remade for 2nd design.

--- web/app/components/App.jsx
+++ web/app/components/App.jsx
@@ -6,16 +6,11 @@ export default class App extends React.Component {
     render() {
         return (
             <div>
-                <MenuTop />
-                <div className="container-fluid">
-                    <div className="row">
-                        <MenuLeft/>
-                        {/*
-                         * React Router chooses children by URL
-                         {this.props.children}
-                         * */}
-                    </div>
-                </div>
+                <MenuLeft/>
+                {/*
+                 * React Router chooses children by URL
+                 {this.props.children}
+                 * */}
             </div>
         );
     }
