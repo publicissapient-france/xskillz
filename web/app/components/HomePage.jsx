fc5d68a306744c1bc7594faf8e31c0c5337ab45c
Evgeny Yurchuk <evgeny.yurchuk@gmail.com>
2016-02-03 01:10:21 +0100

    Home page styling

--- web/app/components/HomePage.jsx
+++ web/app/components/HomePage.jsx
@@ -7,18 +7,57 @@ import Activities from './Activities.jsx';
 export default class HomePage extends React.Component {
     render() {
         return (
-            <div className="container activity">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <div className="page-header lead">
-                            Dernière activité
+            <div className="main">
+                <div className="activity-block timeline-start">
+                    <div className="timeline-wrapper">
+                        <div className="timeline">
+                            <div className="line"></div>
+                            <div className="block">
+                                <span className="ion-ribbon-b"></span>
+                            </div>
                         </div>
-                        <AltContainer stores={[ActivityStore]}
-                                      inject={{items: () => ActivityStore.getState().activities}}>
-                            <Activities />
-                        </AltContainer>
+                        <div className="activity">
+                            <div className="head-text bold">
+                                46 COMPETENCES ACQUISES
+                            </div>
+                            <div className="sub-head-text thin">
+                                PAR LES XEBIANS CE MOIS
+                            </div>
+                            <div className="citation">
+                                “On commence à vieillir quand on finit d’apprendre.”
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <div className="activity-block">
+                    <div className="timeline-wrapper">
+                        <div className="timeline">
+                            <div className="line"></div>
+                            <div className="block bold">
+                                à 12:36
+                            </div>
+                        </div>
+                        <div className="activity">
+                            <div className="photo pull-left">
+                                <div className="wrapper">
+                                    <img className="img-circle img-responsive" src="app/images/thumb2.jpg" alt=""/>
+                                </div>
+                            </div>
+                            <div className="name bold">
+                                NICK FURY
+                            </div>
+                            <div className="company">
+                                Xebia
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*
+                 <AltContainer stores={[ActivityStore]}
+                 inject={{items: () => ActivityStore.getState().activities}}>
+                 <Activities />
+                 </AltContainer>
+                 */}
             </div>
         );
     }
