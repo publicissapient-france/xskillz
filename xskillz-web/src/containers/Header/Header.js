ac17fe834fba82de7f1db48c0a13e1e85f71cceb
Benjamin LACROIX <benjlacroix@gmail.com>
2016-03-05 12:51:53 +0100

    App bar

---
+++ xskillz-web/src/containers/Header/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { routeActions } from 'react-router-redux';
+
+import HeaderContent from '../../components/Header/HeaderContent';
+
+const mapStateToProps = () => {
+    return {}
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        goToSkills: () => dispatch(routeActions.push('/skills')),
+        goToUsers: () => dispatch(routeActions.push('/users'))
+    };
+};
+
+const Header = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(HeaderContent);
+
+export default Header;
