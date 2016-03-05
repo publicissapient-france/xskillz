b410a7407f9c17fdc15e522a078915b52dbff96b
Benjamin LACROIX <benjlacroix@gmail.com>
2016-03-05 14:38:15 +0100

    Deploy to Surge script

--- xskillz-web/scripts/deploy-surge.js
+++ xskillz-web/scripts/deploy-surge.js
@@ -2,9 +2,9 @@ var exec = require('child_process').exec,
     fs = require('fs-extra');
 
 const deployDir = 'deploy/surge';
-const domainName = 'ea-homecare-dev.surge.sh';
+const domainName = 'xskillz-dev.surge.sh';
 
-process.chdir('../ea-homecare-front');
+process.chdir('../xskillz-web');
 
 build = (cb) => {
     console.info('Building front app...');
@@ -21,11 +21,11 @@ copy = () => {
         if (err) return console.error(err);
         fs.emptyDir(deployDir, function (err) {
             if (!err) console.log('success!');
-            fs.copy('dist/', deployDir+'/dist/', function (err) {
+            fs.copy('dist/', deployDir + '/dist/', function (err) {
                 if (err) return console.error(err);
-                fs.copy('index.html', deployDir+'/index.html', function (err) {
+                fs.copy('index.html', deployDir + '/index.html', function (err) {
                     if (err) return console.error(err);
-                    fs.copy(deployDir+'/index.html', deployDir+'/200.html', function (err) {
+                    fs.copy(deployDir + '/index.html', deployDir + '/200.html', function (err) {
                         if (err) return console.error(err);
                         console.info('Successfully copied files');
                         deploy();
@@ -39,7 +39,7 @@ copy = () => {
 deploy = () => {
     console.info('Deploying to Surge...');
 
-    exec('surge -d ' + domainName + ' -p ' + deployDir, (err, stdout) => {
+    exec('node_modules/surge/lib/cli.js -d ' + domainName + ' -p ' + deployDir, (err, stdout) => {
         if (err) throw err;
         console.info(stdout);
     });
