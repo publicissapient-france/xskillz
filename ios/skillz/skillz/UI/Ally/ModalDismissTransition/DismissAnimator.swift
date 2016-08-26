fd3254cfdfeb68445e665f74ff06fee39a3e5d49
Florent Capon <fcapon@xebia.fr>
2016-08-26 17:49:34 +0200

    [iOS] Increase Ally view closure animation.

--- ios/skillz/skillz/UI/Ally/ModalDismissTransition/DismissAnimator.swift
+++ ios/skillz/skillz/UI/Ally/ModalDismissTransition/DismissAnimator.swift
@@ -13,7 +13,7 @@ class DismissAnimator : NSObject {
 
 extension DismissAnimator : UIViewControllerAnimatedTransitioning {
     func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
-        return 0.6
+        return 0.3
     }
     
     func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
